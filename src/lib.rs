d92aa5c3150ee8219999bf61588e58887b7e2296
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2021-12-28 16:02:46 -0300

    tidy up docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,21 @@
-mod counter_one_writer_one_reader;
-mod two_numbers_with_dependency;
+//! # Atomic Story
+//!
+//! Understanding atomics and memory ordering takes time, and it might also be a painful
+//! journey. This is mine.
+//!
+//! I felt that having a repo with simple unit tests would help me enhance my understanding on the topic.
+//! Besides, it's a very compact way of knowledge transfer - that's why I am making this public.
+//!
+//! ## Anatomy
+//!
+//! ### Counter one writer one reader
+//! This is the simples example. Using `Relaxed` is more than enough, since we're only checking the
+//! final value after all threads have incremented the value.
+//!
+//! ### Two numbers with a dependency between them.
+//! This example is more interesting. There are two threads: a writer and a reader. And two values,
+//! `num_a` and `num_b`. This exmample explores different `Ordering` combinations, and how they
+//! can yield different results.
+
+pub mod counter_one_writer_one_reader;
+pub mod two_numbers_with_dependency;
--- src/two_numbers_with_dependency.rs
+++ src/two_numbers_with_dependency.rs
@@ -1,3 +1,4 @@
+//! # Multiple orderings on an example with two threads and two values
 //! Modelling a scenario with two values, a and b. Both a and b are incremented only on a writer thread
 //! and and read on a reader thread.
 //! Invariants
@@ -11,6 +12,76 @@
 //! Notes:
 //! whenever the reader uses `Relaxed` on both operations, then the test fails.
 //!
+//!
+//! ```rust
+//! use std::sync::atomic::Ordering::{Acquire, Relaxed, Release, SeqCst};
+//!
+//! use std::sync::atomic::Ordering;
+//!
+//! use loom::{
+//! sync::{atomic::AtomicUsize, Arc},
+//! thread,
+//! };
+//!
+//!fn two_numbers_with_a_dependency(
+//!    read_ordering_a: Ordering,
+//!    read_ordering_b: Ordering,
+//!    write_ordering_a: Ordering,
+//!    write_ordering_b: Ordering,
+//!) {
+//!    loom::model(move || {
+//!        let num_a = Arc::new(AtomicUsize::new(1));
+//!        let num_b = Arc::new(AtomicUsize::new(0));
+//!
+//!        let num_a2 = num_a.clone();
+//!        let num_b2 = num_b.clone();
+//!        let tb = thread::spawn(move || {
+//!            for idx in 1..4 {
+//!                num_a2.store(idx + 1, read_ordering_a);
+//!                num_b2.store(idx, read_ordering_b);
+//!            }
+//!        });
+//!
+//!        let _ = thread::spawn(move || {
+//!            for _ in 1..4 {
+//!                let nb = num_b.load(write_ordering_b);
+//!                let na = num_a.load(write_ordering_a);
+//!                assert!(na >= nb);
+//!            }
+//!        });
+//!        tb.join().unwrap();
+//!    });
+//!}
+//!
+//!#[test]
+//!#[should_panic]
+//!fn atomics_relaxed_failing() {
+//!    two_numbers_with_a_dependency(Relaxed, Relaxed, Relaxed, Relaxed);
+//!}
+//!
+//!#[test]
+//!#[should_panic]
+//!fn atomics_writer_release_reader_relaxed_failing() {
+//!    two_numbers_with_a_dependency(Release, Release, Relaxed, Relaxed);
+//!
+//!}
+//!
+//!#[test]
+//!fn atomics_writer_release_reader_seqcst_and_relaxed() {
+//!    two_numbers_with_a_dependency(Release, Release, Relaxed, SeqCst);
+//!}
+//!
+//!#[test]
+//!fn atomics_seq_cst_does_not_fail() {
+//!    two_numbers_with_a_dependency(SeqCst, SeqCst, SeqCst, SeqCst);
+//!}
+//!
+//!#[test]
+//!fn atomics_acquire_release_does_not_fail() {
+//!    two_numbers_with_a_dependency(Release, Release, Acquire, Acquire);
+//!}
+//! ```
+
 #[cfg(test)]
 mod tests {
     use std::sync::atomic::Ordering::{Acquire, Relaxed, Release, SeqCst};
